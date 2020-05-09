d7377c30ef366300a221268109266358712bd335
liu <602694077@qq.com>
2020-05-09 14:08:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ld_nfc2/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ld_nfc2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void writeNfc(View view) {
+
+    }
+
+    public void readNfc(View view) {
+
+    }
+}
